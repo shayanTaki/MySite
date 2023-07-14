3b13fcb39a3001bcdea47a3cfce93b31b11d96e8
shayanTaki <takishayan@gmail.com>
2023-07-14 06:14:45 -0700

    add admin page base style for two colums {mtn1 , mtn2}

--- BlogPegasus/views.py
+++ BlogPegasus/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import TexTak
 
 # Create your views here.
-textt = 'shayannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn'
+# textt = 'shayannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn'
 def index(request):
     return render(request, 'blog/index.html')
 
