d274e7c1e812ba3450b229da539b3cf82c4e57f0
shayanTaki <takishayan@gmail.com>
2023-07-14 10:29:27 -0700

    add mtn3 and ok edite for fields

--- BlogPegasus/models.py
+++ BlogPegasus/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 class TexTak(models.Model):
 
-     mtn1 = models.CharField(max_length=200)
-     mtn2 = models.CharField(max_length=200)
+     mtn1 = models.CharField(max_length=400)
+     mtn2 = models.CharField(max_length=400)
+     mtn3 = models.CharField(max_length=400)
 
      def __str__(self):
           return f"{self.mtn1}"
