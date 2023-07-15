2b8211976a51c6220ecee765f2599d5e32ab3f8e
shayanTaki <takishayan@gmail.com>
2023-07-15 13:22:20 -0700

    make new model for menu item and set in  html

--- BlogPegasus/admin.py
+++ BlogPegasus/admin.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import Textarea
-from .models import TexTak
+from .models import TexTak, menuItem
 from django.contrib import admin
 
 class TexTakForm(forms.ModelForm):
@@ -23,4 +23,24 @@ class TexTakAdmin(admin.ModelAdmin):
         extra_context['is_edit'] = True
         return super().change_view(request, object_id, form_url, extra_context=extra_context)
 
+
+class menuItemForm(forms.ModelForm):
+    class Meta:
+        model = menuItem
+        fields = '__all__'
+
+
+
+class menuItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'mtn_kol', 'lnk_a']
+    form = TexTakForm
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['title'] = 'ویرایش مدل'
+        extra_context['is_edit'] = True
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
+
+
 admin.site.register(TexTak, TexTakAdmin)
+admin.site.register(menuItem, menuItemAdmin)
--- BlogPegasus/models.py
+++ BlogPegasus/models.py
@@ -7,3 +7,8 @@ class TexTak(models.Model):
 
      def __str__(self):
           return f"{self.mtn1}"
+class menuItem(models.Model):
+
+     name = models.CharField(max_length=100)
+     mtn_kol = models.CharField(max_length=400)
+     lnk_a = models.CharField(max_length=150)
